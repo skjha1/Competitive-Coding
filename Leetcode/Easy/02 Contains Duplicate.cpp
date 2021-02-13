5f82f4395ef876c08a3ccba8ea6d7151bea449fc
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-02-14 02:02:58 +0530

    Update 02 Contains Duplicate.cpp

--- Leetcode/Easy/02 Contains Duplicate.cpp
+++ Leetcode/Easy/02 Contains Duplicate.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
       
-bool containsDuplicate(vector<int>& nums)
+bool containsDuplicate(vector<int>& nums) // creating a vector varible nums 
     {
-       sort(nums.begin(), nums.end());
-       int n = nums.size();
+       sort(nums.begin(), nums.end()); // sorting the vector 
+       int n = nums.size(); // storing the size of the vector in n 
        
-       for(int i = 1; i < n; i++)
+       for(int i = 1; i < n; i++) // iterating through the whole vector 
        {
-           if(nums[i] == nums[i-1])
+           if(nums[i] == nums[i-1]) // if duplicate found then return true else return false 
            return true;
        }
        
