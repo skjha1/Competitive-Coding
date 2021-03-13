80dc9485317bb356fb168744b76be955acc1c574
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-13 21:22:32 +0530

    Update 01 Two Sum.cpp

--- Leetcode/Easy/01 Two Sum.cpp
+++ Leetcode/Easy/01 Two Sum.cpp
@@ -58,3 +58,29 @@ public:
         return {};
     }
 };
+// ------------------------------------------------------------------------------------------------------------
+
+
+class Solution {
+public:
+    std::vector<int> twoSum(vector<int>& nums, int target) {
+        
+//map for finding the complement of current number in the vector
+        std::unordered_map<int, int> hashmap; 
+        
+        //loop for checking if the complement of the current integer exist in the map
+        for(int i = 0; i < nums.size(); i++){
+            //if complement has found, return current index and index of compliment
+            //index of complement is its value in the map
+			//with C++20, mappy.contains would probably been better
+            if(hashmap.count(target-nums[i])){
+				//return the index of the current integer in the vector, and the index of the complement
+                return {i,(hashmap[target - nums[i]])};
+            }
+            //if complement has not been passed by yet, modify the complement's value with its index
+            hashmap[nums[i]] = i;
+        }
+        //return empty vector if no integers answer found
+        return {};
+}
+};
