95ac64baee3d84f06e59ad3f50c68d60c1c001ef
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-14 10:06:51 +0530

    Update 01 Two Sum.cpp

--- Leetcode/Easy/01 Two Sum.cpp
+++ Leetcode/Easy/01 Two Sum.cpp
@@ -3,6 +3,24 @@
 
 using namespace std;
 
+// ----------------------------------------------------------
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+       for (int i=0;i<nums.size();i++){
+           for (int j=i+1;j<nums.size();j++){
+               int complement =target-nums[i];
+               if (complement == nums[j]){
+                   return {i,j};
+               }
+           }
+       }
+        return {};
+        
+    }
+};
+//---------------------------------------------------------------
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
