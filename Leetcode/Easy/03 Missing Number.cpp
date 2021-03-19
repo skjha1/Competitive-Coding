ae5e94efaa6033ba3592b26f1a1979dbd42fe900
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-19 10:12:47 +0530

    Array O(N)

--- Leetcode/Easy/03 Missing Number.cpp
+++ Leetcode/Easy/03 Missing Number.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) { // creating the variable by the name of nums as a vector 
+    int missingNumber(vector<int>& nums) { // creating the variable by the name of nums as a vector  
     int result = 0;
     for (int i = 0; i < nums.size(); i++) { // iterating through the vector 
         result =result + (i+1)- nums[i];  // getting the missing number and then returning it 
