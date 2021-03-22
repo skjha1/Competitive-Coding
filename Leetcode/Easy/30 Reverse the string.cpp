1db049d41e468bb8e36bb5271cab26ae4e0704a4
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-22 19:58:39 +0530

    string

---
+++ Leetcode/Easy/30 Reverse the string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int i = 0 , j = s.size()-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+};
