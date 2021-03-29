2abf3a2e79e1afe0b672d7cd2182510112c9a2d5
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-03-29 09:39:15 +0530

    Update 23 Middle of the Linked List.cpp

--- Leetcode/Easy/23 Middle of the Linked List.cpp
+++ Leetcode/Easy/23 Middle of the Linked List.cpp
@@ -11,8 +11,8 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* p=head;
-        while (head && head->next)
+        ListNode* p=head; // create a pointer node p and move on the head node 
+        while (head && head->next) // iterate through the list till nodes is there.
         {
             head=head->next->next; //the time p will move one head will move two
             p=p->next; // this way we can take nodes 
