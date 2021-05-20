9f1a172c26244b8a4be87a1eaf870be693a45a93
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-20 12:42:33 +0530

    O(logn)

--- Leetcode/Easy/08 Search Insert Position.cpp
+++ Leetcode/Easy/08 Search Insert Position.cpp
@@ -13,3 +13,28 @@ public:
         return low;
     }
 };
+
+--------------------------------------------------------------------------------------------------------
+    
+    class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n= nums.size();
+        int start=0;
+        int end= n-1;
+        while (start <=end){
+            int mid = start + (end-start)/2;
+            if (target== nums[mid])
+                return mid;
+            else if (target<nums[mid])
+                end=mid-1;
+            else
+            {
+                int res=mid;
+                start=mid+1;
+            }
+            
+        }
+        return start;
+    }
+};
