f4520101b3e85d2f1955e77c828fd7d73fe70884
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-20 23:42:43 +0530

    O(logn)

---
+++ Leetcode/Medium/21 Find Peak Element.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int start=0, end=nums.size();
+        while (start < end) {
+            int mid=start+(end-start)/2;
+            if (mid < nums.size()-1 && nums[mid] < nums[mid+1]) {
+                start=mid+1;
+            }
+            else {
+                end=mid;
+            }
+        }
+        
+        return start;
+    }
+};
