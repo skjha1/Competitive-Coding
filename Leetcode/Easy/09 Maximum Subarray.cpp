fec25624466616641d9edaabdb8f92adc14469c1
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-05 13:43:49 +0530

    Kadane algo : O(n), O(1)

--- Leetcode/Easy/09 Maximum Subarray.cpp
+++ Leetcode/Easy/09 Maximum Subarray.cpp
@@ -12,3 +12,20 @@ public:
         return max_sum;
     }
 };
+--------------------------------------------------------------------------------------------
+    
+    class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0;
+        int maxi=INT_MIN;
+        for (auto i:nums)
+        {
+            sum+=i;
+            maxi=max(maxi,sum); // everytime take maximum after adding the next element 
+            if (sum<0) sum=0; // whenever sum become less then 0 set its value to zero 
+        }
+        return maxi;
+    }
+};
+// tc sc =O(n), O(1)
