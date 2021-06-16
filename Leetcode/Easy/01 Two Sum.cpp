bde9f77b4888c37736881207095a22319485e063
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-16 23:10:25 +0530

    Update 01 Two Sum.cpp

--- Leetcode/Easy/01 Two Sum.cpp
+++ Leetcode/Easy/01 Two Sum.cpp
@@ -139,3 +139,20 @@ public:
         return {};
     }
 };
+
+------------------------------------------------------------------------------------------------------------------------
+	
+	class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        
+        for(int i = 0; i < nums.length; i++)
+            map.put(nums[i], i);
+        
+        for(int i = 0; i < nums.length; i++)
+            if(map.containsKey(target - nums[i]) && i != map.get(target - nums[i]))
+                return new int[] {i, map.get(target - nums[i])};
+        
+        return new int[2];
+    }
+}
