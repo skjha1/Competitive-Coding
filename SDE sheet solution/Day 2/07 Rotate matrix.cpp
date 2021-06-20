c2504c1fa0bdc2af17d7989ce36d552ece61357c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-20 12:34:00 +0530

    Update 07 Rotate matrix.cpp

--- SDE sheet solution/Day 2/07 Rotate matrix.cpp
+++ SDE sheet solution/Day 2/07 Rotate matrix.cpp
@@ -1,3 +1,6 @@
+// https://www.geeksforgeeks.org/inplace-rotate-square-matrix-by-90-degrees/
+// solution : https://uploads.disquscdn.com/images/a2bee3cd55348623b01e27c191b7dc79a53de1dabbc7a64e2619bcec6e391acb.png
+// This is ratate image fron leetcode
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
