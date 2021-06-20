c2443295a2f7353166732bbb6cc2f40f54e34ef4
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-20 12:30:06 +0530

    Create 07 Rotate matrix.cpp

---
+++ SDE sheet solution/Day 2/07 Rotate matrix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n= matrix.size();
+        // this loops are for the transpose if the matrix 
+        for (int i=0;i<n;i++){
+            for (int j=i;j<n;j++){
+                int temp= matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for (int i=0;i<n;i++){ // this loop is to flip the matrix horizontally
+            for (int j=0;j<(n/2);j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-1-j];
+                matrix[i][n-1-j]=temp;
+            }
+        }
+    }
+};
