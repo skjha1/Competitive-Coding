3e146def732bf3e7b596a1035c7673e4318af1f6
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-20 12:21:12 +0530

    Delete 05 Best Time to Buy and Sell Stock I.cpp

--- SDE sheet solution/Day 2/05 Best Time to Buy and Sell Stock I.cpp
+++
@@ -1,9 +0,0 @@
-class Solution {
-public:
-    int maxProfit(vector<int> &prices) {
-    int ret = 0;
-    for (int p = 1; p < prices.size(); ++p) 
-      ret += max(prices[p] - prices[p - 1], 0);    
-    return ret;
-}
-};
