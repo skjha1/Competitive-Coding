8b05989c45d7db03c5668ad6cc9b0b763d96c968
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-06-26 19:54:08 +0530

    Update 03 Missing Number.cpp

--- Leetcode/Easy/03 Missing Number.cpp
+++ Leetcode/Easy/03 Missing Number.cpp
@@ -8,3 +8,19 @@ public:
     return result;
 }
 };
+-------------------------------------------------
+    
+    class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n=nums.size();
+        int xor1=0;
+        int xor2=0;
+       for (int i=0;i<n;i++)
+       {
+           xor1^=nums[i];  // let say array is 3,0,1 then bit1 will store  3^0^1 
+           xor2^=(i+1); // this will store 1,2,3
+       }
+        return xor1^xor2; // a^a=0 ,so the number present both in x1 and x2 will cancel out(become zero ) and the element which is not present in x1 . i.e 2 will be returned by the function.
+    }
+};
