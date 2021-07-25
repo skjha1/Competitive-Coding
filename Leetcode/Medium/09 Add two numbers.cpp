4b180e6f576cff5c6d7de67875f5dda4b73e0417
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-07-25 19:27:19 +0530

    Update 09 Add two numbers.cpp

--- Leetcode/Medium/09 Add two numbers.cpp
+++ Leetcode/Medium/09 Add two numbers.cpp
@@ -1,3 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode preHead(0);
+        ListNode *p=&preHead;
+        int extra=0;
+        while (l1 || l2 || extra)
+        {
+            if (l1)
+            {
+                extra+=l1->val;
+                l1=l1->next;
+            }
+            if (l2)
+            {
+                extra+=l2->val;
+                l2=l2->next;
+            }
+            p->next= new ListNode(extra%10);
+            extra/=10;
+            p=p->next;
+        }
+        return preHead.next;
+    }
+};
+--------------------------------------------------------------------------------------
 // https://leetcode.com/problems/add-two-numbers/
 
 /**
