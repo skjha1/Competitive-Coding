8d4bcf167e320b8d27e4d1b18191206cb2fdd663
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2023-01-01 23:11:19 +0530

    Create 01 two_sum.java

---
+++ Leetcode/Easy/01 two_sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int index[] = {-1,-1};
+        for (int i=0; i<nums.length-1; i++)
+        {
+            for (int j=i+1; j<nums.length; j++)
+            {
+                if (target == nums[i]+nums[j]){
+                    index[0] = i;
+                    index[1] = j;  
+                }
+            }
+        }
+        return index;
+    }
+}
