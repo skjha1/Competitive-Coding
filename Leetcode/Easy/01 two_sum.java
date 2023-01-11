183a34e9f589091534bbd5e2f8fb7a32e7e29f65
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2023-01-12 00:12:09 +0530

    Update 01 two_sum.java

--- Leetcode/Easy/01 two_sum.java
+++ Leetcode/Easy/01 two_sum.java
@@ -3,7 +3,7 @@ class Solution {
         int index[] = {-1,-1};
         for (int i=0; i<nums.length-1; i++)
         {
-            for (int j=i+1; j<nums.length; j++)
+            for (int j=i+1; j<=nums.length; j++)
             {
                 if (target == nums[i]+nums[j]){
                     index[0] = i;
